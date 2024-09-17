a11910f56676c3ec02d6b55909b5a48e63331260
Dave Farinelli <d@dfar.io>
2024-09-17 12:41:53 +0000

    Add version information

---
+++ inject-version.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const childProcess = require("child_process");
+
+const dateKey = "~DATE~";
+const gitKey = "~GIT_SHA~";
+const templateFile = "./src/environments/version.template";
+const outputFile = "./src/environments/version.ts";
+
+function replace(key, value) {
+  const data = fs.readFileSync(outputFile, "utf8");
+  const regex = new RegExp(key, "g");
+  const result = data.replace(regex, value);
+
+  fs.writeFileSync(outputFile, result, "utf8");
+}
+
+fs.copyFile(templateFile, outputFile, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    childProcess.exec("git rev-parse --short HEAD", function(childErr, sha) {
+      if (childErr) {
+        console.log(childErr);
+      } else {
+        replace(gitKey, sha.trim());
+
+        var datetime = new Date().toISOString()
+                                 .slice(0,16)
+                                 .replace('T','|')
+                                 .replaceAll('-', '.');
+          replace(dateKey, datetime.trim());
+      }
+    });
+  }
+});
---
+++ src/environments/version.ts
@@ -0,0 +1,5 @@
+// Only edit the template file, do not edit version.ts directly.
+// value defaults to: "VERSION NOT SET" to indicate it has not been changes.
+export const buildConstants = {
+  VERSION: "VERSION NOT SET"
+};
