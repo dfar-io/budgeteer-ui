5b66cb31260c8d7b3d9cad0c385a0f0cbbc5cc63
Dave Farinelli <d@dfar.io>
2024-09-29 15:22:52 -0400

    Update version.ts

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,5 +1,4 @@
 // Only edit the template file, do not edit version.ts directly.
-// value defaults to: "NOT SET" to indicate it has not been changed.
 export const buildConstants = {
   SHA: "SHA",
   URL: "URL"
